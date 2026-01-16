1824afd600246c5eca7e3f0065a2fbccef3eca31
CookieKing1016 <23307100078@m.fudan.edu.cn>
2026-01-16 23:55:29 +0800

    Lab2-finished

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -12,9 +12,11 @@ public class SquarePrimesTest {
      */
     @Test
     public void testSquarePrimesSimple() {
-        IntList lst = IntList.of(14, 15, 16, 17, 18);
-        boolean changed = IntListExercises.squarePrimes(lst);
-        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
-        assertTrue(changed);
+        IntList lst1 = IntList.of(14, 15, 16, 17, 18);
+        IntList lst2 = IntList.of(3,5,7,14,17);
+        IntListExercises.squarePrimes(lst1);
+        IntListExercises.squarePrimes(lst2);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst1.toString());
+        assertEquals("9 -> 25 -> 49 -> 14 -> 289", lst2.toString());
     }
 }
